6375a5bb5f49e3af1a20fc471f26ac8ba7e8c7ea
KevinUTAT <wenkai.xu@mail.utoronto.ca>
2020-11-21 17:42:35 -0500

    ADS: resizable bbox, no effect yet

---
+++ vision/Ancker.py
@@ -0,0 +1,55 @@
+import sys
+from os import listdir, system, execl
+import os.path
+import PySide2
+import copy
+from PySide2.QtWidgets import (QGraphicsScene, 
+                        QGraphicsRectItem)
+from PySide2.QtGui import (QBrush, QPen, QFont)
+from PySide2.QtCore import QLineF, QPointF
+
+
+class Ancker(QGraphicsRectItem):
+# Ancker for a BBox corner
+    def __init__(self, x, y, w, h, brush, atype, bbox_ref, parent=None):
+        super().__init__(x, y, w, h, parent=parent)
+        super().setBrush(brush)
+
+        # atype: tl, tr, bl, br
+        self.atype = atype
+        self.bbox = bbox_ref
+        self.origin = QPointF(x, y)
+        self.xywh = [x, y, w, h] # x, y is TL corner
+
+
+    def mouseMoveEvent(self, event):
+        super().mouseMoveEvent(event)
+        new_pos = self.scenePos()
+        # move other anckers acrodingly
+        if self.atype == 'tl':
+            self.bbox.tr.setY(new_pos.y())
+            self.bbox.bl.setX(new_pos.x())
+        elif self.atype == 'tr':
+            self.bbox.tl.setY(new_pos.y())
+            self.bbox.br.setX(new_pos.x())
+        elif self.atype == 'bl':
+            self.bbox.tl.setX(new_pos.x())
+            self.bbox.br.setY(new_pos.y())
+        elif self.atype == 'br':
+            self.bbox.tr.setX(new_pos.x())
+            self.bbox.bl.setY(new_pos.y())
+        else:
+            print("Ancker Error: Unknow type " + self.atype)
+        # move lines
+        self.bbox.redraw_lines_by_anckers()
+
+    def abs_scenePos(self):
+        # TL corner
+        return self.origin + self.pos()
+
+
+    def abs_scenePos_center(self):
+        TL = self.abs_scenePos()
+        TL.setX(TL.x() + self.xywh[2]/2)
+        TL.setY(TL.y() + self.xywh[3]/2)
+        return TL
--- vision/bbox.py
+++ vision/bbox.py
@@ -1,14 +1,17 @@
 import sys
 from os import listdir, system, execl
 import os.path
+import copy
 import PySide2
 from PySide2.QtWidgets import QGraphicsScene
 from PySide2.QtGui import QBrush, QPen, QFont
-
+from PySide2.QtCore import QLineF, QPointF
+from Ancker import Ancker
 
 
 class BBox(object):
     def __init__(self, xywh, imgSizeWH, class_num):
+        self.parent = None
         # make sure xywh = [centerX, centerY, width, height]
         self.xywh = xywh
         self.imgSizeWH = imgSizeWH
@@ -26,24 +29,74 @@ class BBox(object):
         # then this function will drew on the scene
         blueBrush = QBrush(PySide2.QtCore.Qt.blue)
         bluePen = QPen(PySide2.QtCore.Qt.blue)
-        highlightPen = QPen(PySide2.QtCore.Qt.blue)
-        highlightPen.setWidth(5)
+        highlightPen = QPen(PySide2.QtCore.Qt.red)
+        highlightPen.setWidth(3)
 
-        tl = scene_ref.addRect(self.left-5, self.top-5, 10,10, brush=blueBrush)
-        tr = scene_ref.addRect(self.right-5, self.top-5, 10,10, brush=blueBrush)
-        bl = scene_ref.addRect(self.left-5, self.bottom-5, 10,10, brush=blueBrush)
-        br = scene_ref.addRect(self.right-5, self.bottom-5, 10,10, brush=blueBrush)
+        # 4 ancker points at 4 corners
+        # tl = scene_ref.addRect(self.left-5, self.top-5, 10,10, brush=blueBrush)
+        # tl.setFlag(PySide2.QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        # tr = scene_ref.addRect(self.right-5, self.top-5, 10,10, brush=blueBrush)
+        # tr.setFlag(PySide2.QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        # bl = scene_ref.addRect(self.left-5, self.bottom-5, 10,10, brush=blueBrush)
+        # bl.setFlag(PySide2.QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        # br = scene_ref.addRect(self.right-5, self.bottom-5, 10,10, brush=blueBrush)
+        # br.setFlag(PySide2.QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        self.tl = Ancker(self.left-5, self.top-5, 10,10, blueBrush, 'tl', self)
+        self.tl.setFlag(PySide2.QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        scene_ref.addItem(self.tl)
+        self.tr = Ancker(self.right-5, self.top-5, 10,10, blueBrush, 'tr', self)
+        self.tr.setFlag(PySide2.QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        scene_ref.addItem(self.tr)
+        self.bl = Ancker(self.left-5, self.bottom-5, 10,10, blueBrush, 'bl', self)
+        self.bl.setFlag(PySide2.QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        scene_ref.addItem(self.bl)
+        self.br = Ancker(self.right-5, self.bottom-5, 10,10, blueBrush, 'br', self)
+        self.br.setFlag(PySide2.QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        scene_ref.addItem(self.br)
+
 
         if highlight:
-            top_line    = scene_ref.addLine(self.left, self.top, self.right, self.top, pen=highlightPen)
-            bottom_line = scene_ref.addLine(self.left, self.bottom, self.right, self.bottom, pen=highlightPen)
-            left_line   = scene_ref.addLine(self.left, self.top, self.left, self.bottom, pen=highlightPen)
-            right_line  = scene_ref.addLine(self.right, self.top, self.right, self.bottom, pen=highlightPen)
+            self.top_line    = scene_ref.addLine(\
+                self.left, self.top, self.right, self.top, pen=highlightPen)
+            self.bottom_line = scene_ref.addLine(\
+                self.left, self.bottom, self.right, self.bottom, pen=highlightPen)
+            self.left_line   = scene_ref.addLine(\
+                self.left, self.top, self.left, self.bottom, pen=highlightPen)
+            self.right_line  = scene_ref.addLine(\
+                self.right, self.top, self.right, self.bottom, pen=highlightPen)
         else:
-            top_line    = scene_ref.addLine(self.left, self.top, self.right, self.top, pen=bluePen)
-            bottom_line = scene_ref.addLine(self.left, self.bottom, self.right, self.bottom, pen=bluePen)
-            left_line   = scene_ref.addLine(self.left, self.top, self.left, self.bottom, pen=bluePen)
-            right_line  = scene_ref.addLine(self.right, self.top, self.right, self.bottom, pen=bluePen)
+            self.top_line    = scene_ref.addLine(\
+                self.left, self.top, self.right, self.top, pen=bluePen)
+            self.bottom_line = scene_ref.addLine(\
+                self.left, self.bottom, self.right, self.bottom, pen=bluePen)
+            self.left_line   = scene_ref.addLine(\
+                self.left, self.top, self.left, self.bottom, pen=bluePen)
+            self.right_line  = scene_ref.addLine(\
+                self.right, self.top, self.right, self.bottom, pen=bluePen)
+
+
+    def redraw_lines_by_anckers(self):
+        # redraw lines by connecting anckers
+        self.top_line.setLine(\
+            self.tl.abs_scenePos_center().x(),\
+            self.tl.abs_scenePos_center().y(),\
+            self.tr.abs_scenePos_center().x(),\
+            self.tr.abs_scenePos_center().y())
+        self.bottom_line.setLine(\
+            self.bl.abs_scenePos_center().x(),\
+            self.bl.abs_scenePos_center().y(),\
+            self.br.abs_scenePos_center().x(),\
+            self.br.abs_scenePos_center().y())
+        self.left_line.setLine(\
+            self.tl.abs_scenePos_center().x(),\
+            self.tl.abs_scenePos_center().y(),\
+            self.bl.abs_scenePos_center().x(),\
+            self.bl.abs_scenePos_center().y())
+        self.right_line.setLine(\
+            self.tr.abs_scenePos_center().x(),\
+            self.tr.abs_scenePos_center().y(),\
+            self.br.abs_scenePos_center().x(),\
+            self.br.abs_scenePos_center().y())
 
 
         # return a string representing one line of label
