3c261209a8f13711263790adcdf65f9dfa9a4b12
KevinUTAT <wenkai.xu@mail.utoronto.ca>
2021-05-29 14:22:52 -0400

    Create a tester

---
+++ tester/README.md
@@ -0,0 +1,8 @@
+# Visual Tester
+This modal is used to test the surviver game ai as well as it's computer vision modula. \
+This modal contains a simple video player to play pre recorded game paly videos and track and visualizing game controls
+# How to run the tester
+The tester works with tha same python environment as the main project, but you might need to install video codecs to be able to play back the videos. \
+The easiest way is install K-Lite here : https://www.codecguide.com/download_kl.htm \
+\
+Run the tester at full screen in the same time as the main project.
---
+++ tester/visual_tester.py
@@ -0,0 +1,64 @@
+import sys
+from os import listdir, system
+import os.path
+import os
+import PySide2
+from PySide2 import QtGui
+from PySide2 import QtWidgets
+from PySide2 import QtXml
+from PySide2.QtMultimedia import QMediaContent, QMediaPlayer
+from PySide2.QtMultimediaWidgets import QVideoWidget
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import (QApplication, QPushButton, QAction,
+                            QFileDialog, QLabel, QGridLayout)
+from PySide2.QtCore import QFile, QObject, QRectF, Qt, QUrl
+from PySide2.QtGui import (QIcon, QPixmap, QImage, QCursor, QKeySequence)
+
+# Main window for the visual tester 
+class VTester(QObject):
+
+    def __init__(self, ui_file, parent=None):
+        super(VTester, self).__init__(parent)
+        ui_file = QFile(ui_file)
+        ui_file.open(QFile.ReadOnly)
+        
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+        ui_file.close()
+
+        # setup the video player
+        self.player_widget = QVideoWidget()
+        self.player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+
+        self.window.findChild(QGridLayout, "gridLayout").\
+            addWidget(self.player_widget, 0, 0)
+
+        self.player.setVideoOutput(self.player_widget)
+
+        # open video action
+        self.window.findChild(QAction, "actionOpen_video").\
+            triggered.connect(self.load_video_from_dialog)
+
+    def show(self):
+        self.window.show()
+
+
+    def load_video_from_dialog(self):
+        (file_dir, ext) = QFileDialog.getOpenFileName(\
+                filter="Video files (*.mp4)")
+        if file_dir != '':
+            self.player.setMedia(
+                QMediaContent(QUrl.fromLocalFile(file_dir)))
+            self.player.play()
+
+
+
+
+
+if __name__ == '__main__':
+    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+    app = QApplication(sys.argv)
+    app.setAttribute(Qt.AA_EnableHighDpiScaling)
+    tester_window = VTester('visualtester.ui')
+    tester_window.show()
+    sys.exit(app.exec_())
